1649def1f1eda46a93cfec415f2f4800dfc6fa59
Neeraj Chandra <60702205+imchandra19@users.noreply.github.com>
2023-05-29 22:45:40 +0530

    Update receive_stream.py
    Port 80

--- python/receive_stream.py
+++ python/receive_stream.py
@@ -30,7 +30,7 @@ async def handle_connection(websocket, path):
             break
 
 async def main():
-    server = await websockets.serve(handle_connection, '0.0.0.0', 3001)
+    server = await websockets.serve(handle_connection, '0.0.0.0', 80)
     await server.wait_closed()
 
 asyncio.run(main())
